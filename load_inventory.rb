00e715fd1c6d5b5688da7e54d5bbc3738302528b
nichol alexander <nichol.alexander@gmail.com>
2015-08-25 10:29:15 -0400

    stub load_inventory

---
+++ load_inventory.rb
@@ -0,0 +1,29 @@
+# load_inventory.rb <filename>
+
+# This utility is responsible for determining the format
+# of the incoming inventory data file, parsing it, and
+# exporting it to our master inventory file.
+# 
+# The parse methods return a hash of our inventory.
+
+
+
+def check_supplier_format(file)
+end
+
+def parse_csv(file)
+end
+
+def parse_pipe(file)
+end
+
+def write_to_inventory(data, file)
+end
+
+def main
+  puts "loading the inventory"
+  puts "please be patient"
+
+end
+
+main
